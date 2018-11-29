cf3a8eb72b2ca9aad1bf000d9705463a4e3c351f
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-11-28 22:18:21 -0600

    No estoy seguro de si todos los modos funcionan, pero el low lo hace, no tengo tiempo para seguir porgramando hoy, pero me gustaría umu

--- filesearcher.py
+++ filesearcher.py
@@ -1,29 +1,61 @@
-'''#-*-coding: utf-8-*-
-from sys import argv
-from os import chdir, getcwd, listdir, system
+#-*-coding: utf-8-*-
+from sys import argv 
+from os import chdir, getcwd, listdir, system, path
 from platform import python_version as pv
 from platform import platform
 from threading import *
-
 class Searcher():
 	def __init__(self):
 		self.od = getcwd() #od = Original directory
-	def find(self, f ,rt = "none"):
+	def find(self,d,f ,rt = "none"):
+		chdir(d)
 		files = listdir()
-		dirs = []
+		fobjs = [getcwd()]
 		for file in files:
-			if file == isdir: #tengo que buscar como ver si esto es un dir o no en python.
-				
-		    else:
-		    	if file == f:
-		    		if rt == "none":
-		    			print("''{}'' found in {}".format(f))
+			if rt == "none":
+				if file == f:
+					return True
+			else:
+				if rt == "getdirs":
+					if path.isdir(file):
+						fobjs.append("{}\\{}".format(getcwd(),file))
+				elif rt == "getfiles":
+					if path.isfile(file):
+						fobjs.append(file)
 	def low(self, f):
-		find()
+		if self.find(getcwd(), f):
+			print("File '{}' found in {}".format(f, getcwd()))
+		else:
+			print("File '{}' not found".format(f))
 	def nth(self, f):
-		pass
+		dirs = self.find("none", "getdirs")
+		for d in dirs:
+			self.find("{}\\{}".format(getcwd(), d) ,f)
+	def gointo(self, d, f):
+		rtd = []
+		for file in listdir(d):
+			if path.isdir(file):
+				rtd.append(file)
+			else:
+				if file == f:
+					return (d, True)
+		return rtd
 	def h(self, f):
-		pass
+	    dirs = self.find(getcwd(), "none", "getdirs")
+	    while True:
+	    	c = self.dirlist(dirs)
+	    	if c[1] == True:
+	    		print("File '{}' found in {}".format(f, c[0]))
+	    		break
+	    	else:
+	    		dirs = c
+	def dirlist(self, lst):
+	    for d in lst:
+	    	comp = self.gointo(d, f)
+	    	if comp[1] == True: 	
+	    		return comp	
+	    return comp
+
 	def search(self, f, category="l"):
 		if category == "l":
 			t = self.low
@@ -34,8 +66,6 @@ class Searcher():
 		search = Thread(target=target, args=(f,))
 		search.daemon = True
 		search.start()
-
-		
 	def console(self):
 		if str(pv())[0] == "3":
 			raw_input = input
@@ -52,7 +82,11 @@ class Searcher():
 				elif cmd[:2] == "cd":
 					chdir(cmd[3:])
 				elif cmd == "dir":
-					for file in 
+					for file in listdir():
+						if path.isdir(file):
+							print("[dir] {}".format(file))
+						else:
+							print(file)
 				elif cmd[:9] == "lowsearch":
 					search(cmd[10:], "l")
 				elif cmd[:12] == "middlesearch":
@@ -65,18 +99,38 @@ class Searcher():
 					print("Command ''{}'' not found.".format(cmd))
 			except Exception as e:
 				print(e)
-
-
 if __name__ == '__main__':
 	main = Searcher()
 	count = 0
+	file = ""
+	cat = ""
+	ogs = True
 	for arg in argv:
-		if argv[0] != "-":
+		if arg [0] != "-":
 			count+=1
 			continue
-		elif argv=="-console":
-			main.console()
+		if arg=="-console":
+			print("Starting Console")
+			ogs = False
 			break
-
-
-'''
+		elif arg == "-file":
+			print("File declared")
+			file = argv[count +1]
+		elif arg == "-m":
+			print("Mode declared")
+			cat = argv[count + 1]
+		else:
+			print("[Error] arg '{}' not found".format(arg))
+			exit()
+		count += 1
+	if ogs == True:
+		if cat == "h":
+			main.h(file)
+		elif cat == "nth":
+			main.nth(file)
+		elif cat == "low":
+			main.low(file)
+		else:
+			print("[Error] '{}' mode not found".format(cat))
+	else:
+		main.console()
